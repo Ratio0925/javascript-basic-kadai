166ab9d44641368ff8af6ef47eab138b508fc9c9
Ratio0925 <141377143+Ratio0925@users.noreply.github.com>
2023-08-21 11:55:52 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,7 @@
-const bodyWeight = 68;
+let bodyWeight = 68;
 
-const height = 1.7;
+let height = 1.7;
 
-console.log(bodyWeight / (height * height))
+let bmi = bodyWeight / (height*height)
+
+console.log(bmi)
